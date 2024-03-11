3a2ad52883ec65845239db3ce66aa053046d90c3
MatthewPCope <matthew.p.cope@gmail.com>
2024-03-11 07:46:21 -0700

    adding move item feature

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,22 @@
-import { db } from "../firebase/config";
-import { doc, deleteDoc } from "firebase/firestore";
+import { useState } from "react";
+// import { db } from "../firebase/config";
+// import { doc, deleteDoc } from "firebase/firestore";
 
 export default function ItemList({needs}) {
 
-    const handleClick = async (id) => {
-        await deleteDoc(doc(db, 'needs', id))
-    }
+    const [wants, setWants] = useState([])
+    const [movedNeeds, setMovedNeeds] = useState([])
+
+    // const handleClick = async (id) => {
+    //     await deleteDoc(doc(db, 'needs', id))
+    // }
+
+    const moveNeed = (need) => {
+        setWants(wants.filter(i => i !== need)); // Remove clicked item from the first list
+        setMovedNeeds([...movedNeeds, need]); // Add clicked item to the second list
+    };
+
+
     
     return (
         <div className="form">
@@ -13,7 +24,7 @@ export default function ItemList({needs}) {
             <div className="table-scroll">
                 <ul>
                     {needs.map(need => (
-                        <li className="list" key={need.id} onClick={() => handleClick(need.id)}>{need.name}</li>
+                        <li className="list" key={need.id} onClick={() => moveNeed(need.id)}>{need.name}</li>
                     ))}
                 </ul>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,18 @@ import  { useCollection } from '../hooks/useCollection'
 import ItemList from '../components/ItemList'
 import GotList from '../components/GotList'
 import { useAuthContext } from '../hooks/useAuthContext'
-
+// import React, { useState } from 'react';
 
 
 export default function Home() {
+
+  //   const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']); // Initial items in the first list
+  //   const [movedItems, setMovedItems] = useState([]); // Items that have been moved to the second list
+
+  //   const moveItem = (item) => {
+  //     setItems(items.filter(i => i !== item)); // Remove clicked item from the first list
+  //     setMovedItems([...movedItems, item]); // Add clicked item to the second list
+  // };
 
     const { user } = useAuthContext()
     const { documents: needs, error} = useCollection(
